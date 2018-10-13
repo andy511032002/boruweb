12bdbb614090a37f1613714db581089e8848f209
Boru <andy511032002@gmail.com>
2018-10-14 02:53:08 +0800

    about update
    about update

--- components/Home/index.js
+++ components/Home/index.js
@@ -14,9 +14,9 @@ class Home extends React.Component{
   }
   render(){
     return(
-      <div >
+      <div style={{fontFamily:'irohamaru-Regular'}}>
         <h1>Testing...</h1>
-        <p>這是用來測試GitHub Page以及練習React的網站</p>
+        <p>這是用來測試GitHub Page以及練習React的網站，仍有很多東西還在學習，敬請見諒。</p>
         <p>目前還在施工中...</p>
       </div>
 
